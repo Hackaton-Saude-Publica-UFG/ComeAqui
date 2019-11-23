f124d8af77ca0ce13a83318e4b8d0a32289e6873
Rebecca Moraes <rebecca@ufg.br>
2019-11-23 08:07:26 -0300

    Adicionando barra de menu

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,63 @@ import 'package:flutter/material.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
+  final appTitle = 'ComeAqui';
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ComeAqui',
+      title: appTitle,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: HomePage(),
+      home: HomePage(title: appTitle),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
+  final String title;
+
+  HomePage({Key key, this.title}) : super(key: key);
+
+  //_TabbedPageState createState() => _TabbedPageState();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Theme.of(context).primaryColor,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.location_on),
+              title: Text('Mapa'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_offer),
+              title: Text('Ofertas'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.help),
+              title: Text('Infos'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Infos'),
+            ),
+          ],
+          /*currentIndex: _selectedIndex,*/
+          selectedItemColor: Colors.white,
+          /*onTap: _onItemTapped,*/
+        ),
+      ),
     );
   }
 }
+
+//class _TabbedPageState extends State<
